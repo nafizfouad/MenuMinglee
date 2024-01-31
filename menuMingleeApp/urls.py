f54d0ed7ccf50c8a6038619469ba501947e54cca
Calculator <nafizfouad@gmail.com>
2024-01-31 15:07:12 +0600

    Initial Commit

---
+++ menuMingleeApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
+urlpatterns = [
+    path('',views.home,name='home'),
+
+]
